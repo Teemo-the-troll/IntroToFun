ace552c9b20a5ea555dab49d231429c94c12c72b
Teemo-the-troll <mikulda@ka.cz>
2021-11-05 17:18:39 +0100

    cnb and perfect numss

--- src/main/kotlin/perfectNums.kt
+++ src/main/kotlin/perfectNums.kt
@@ -1,19 +1,22 @@
 import java.util.*
+import kotlin.math.*
 
 
 fun main() {
-    Scanner(System.`in`).use() { sc ->
+    Scanner(System.`in`).use { sc ->
         println("Number N")
-        var input = sc.nextInt();
-/*
-        for () {
-
-        }*/
-
-
-
-
+        val input = sc.nextDouble()
+        for (number in 0..input.toInt()) {
+            var i = 1
+            var sumOfFactors = 0
+            while (i <= floor(sqrt(number.toDouble()))) {
+                if (number % i == 0)
+                    sumOfFactors += i + (number / i)
+                i++
+            }
+            if (sumOfFactors.toDouble() / 2 == number.toDouble())
+                println(number)
+        }
     }
-
 }
 
